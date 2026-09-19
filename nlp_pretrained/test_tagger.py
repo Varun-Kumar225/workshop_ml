dabec17869f4278f2c07dfe31436355fce00de08
Varun-Kumar225 <varunkumarmitcse@gmail.com>
2026-09-19 10:05:18 +0530

    nlp_started

---
+++ nlp_pretrained/test_tagger.py
@@ -0,0 +1,32 @@
+from nlp_pretrained.ner_tagger import extract_entities
+import nltk
+
+try:
+    nltk.data.find('chunkers/maxent_ne_chunker_tab')
+except LookupError:
+    nltk.download('maxent_ne_chunker_tab')
+
+# Test 1
+text1 = "Apple is in London"
+result1 = extract_entities(text1)
+
+print("Text:", text1)
+print("Entities:", result1)
+print()
+
+
+# Test 2
+text2 = "Barack Obama was born in Hawaii"
+result2 = extract_entities(text2)
+
+print("Text:", text2)
+print("Entities:", result2)
+print()
+
+
+# Test 3
+text3 = "Google was founded by Larry Page in California"
+result3 = extract_entities(text3)
+
+print("Text:", text3)
+print("Entities:", result3)
